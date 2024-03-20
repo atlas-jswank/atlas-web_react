c2f425b1652556dc3a44ad9da6c83ce59bc11476
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-03-20 10:00:54 -0500

    Add task_1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import debounce from 'lodash.debounce';
+
+function element({ e, text, id }) {
+  const elm = document.createElement(e);
+  elm.innerHTML = text;
+  if (id) {
+    elm.id = id;
+  }
+  return elm;
+}
+
+const p = ({ id, text }) => element({ e: 'p', text, id });
+const button = (text) => element({ e: 'button', text });
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+function updateCounter() {
+  count++;
+  const elm = document.getElementById('count');
+  elm.innerHTML = `${count} clicks on the button`;
+}
+
+document.getElementsByTagName('button')[0].onclick = debounce(
+  updateCounter,
+  500
+);
