ea75ed6cd3c47c8d2f60a27bb63aac92c6de15f9
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-03-20 09:54:31 -0500

    Fix formatting

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,11 +1,5 @@
-import $ from "jquery";
+import $ from 'jquery';
 
-function p(text) {
-  const elm = document.createElement("p");
-  elm.innerHTML = text;
-  return elm;
-}
-
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
